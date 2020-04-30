65285e766c68f71be4fbc0f3d7feb1a49e91ad61
Prasheel Soni <prasheelsoni11@gmail.com>
2020-04-30 22:21:23 +0530

    enable cors on server

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./Day11-Static Page Server/routes/index');
 const day12router = require('./Day12-Email Notification System/send-email');
@@ -14,7 +15,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
