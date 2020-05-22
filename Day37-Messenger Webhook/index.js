0bac30ecf3a1d2057b4b7d8a963c72a8b60271e4
Prasheel Soni <prasheelsoni11@gmail.com>
2020-05-22 19:50:09 +0530

    add get support

--- Day37-Messenger Webhook/index.js
+++ Day37-Messenger Webhook/index.js
@@ -3,9 +3,7 @@ const router = express.Router();
 
 
 router.post('/', (req, res) => {
-
 let body = req.body;
-
 // Checks this is an event from a page subscription
 if (body.object === 'page') {
 
@@ -24,6 +22,33 @@ if (body.object === 'page') {
     // Returns a '404 Not Found' if event is not from a page subscription
     res.sendStatus(404);
 }
+});
+
+router.get('/', (req, res) => {
+
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = "100daysofcode";
+
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+
+        // Checks the mode and token sent is correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+            // Responds with the challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
+    }
 });
 
 module.exports = router;
