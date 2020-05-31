80264edbb895e4d015f0e6890bf12df2c97472ae
Prasheel Soni <prasheelsoni11@gmail.com>
2020-05-31 18:08:08 +0530

    edit gitignore

---
+++ Day44-Article Viewer/article-viewer/build/precache-manifest.ababae26b00ff889894704ba85180d92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25784bbc36a92839b0e2b2d99920bea6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "832f26aa6bd04180a875",
+    "url": "/static/css/main.0594bc09.chunk.css"
+  },
+  {
+    "revision": "5e926c88e3be0fab35ad",
+    "url": "/static/js/2.640605bb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.640605bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "832f26aa6bd04180a875",
+    "url": "/static/js/main.0f10b399.chunk.js"
+  },
+  {
+    "revision": "e11278bbbdf4a39d08bd",
+    "url": "/static/js/runtime-main.de438884.js"
+  }
+]);
---
+++ Day46-Admin Panel/admin-panel/build/precache-manifest.fb0f546e36cf1fe352c3d3f26f302912.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcf3a05990826694238bfbc4af04815f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e2a6d4ea25eb9ac6633",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "795c8b9f687626616d69",
+    "url": "/static/js/2.ca3c0705.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.ca3c0705.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e2a6d4ea25eb9ac6633",
+    "url": "/static/js/main.46216b1e.chunk.js"
+  },
+  {
+    "revision": "51de7792aa9ddd1a4481",
+    "url": "/static/js/runtime-main.da36c362.js"
+  }
+]);
